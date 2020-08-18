e71dc392160a63415521655983bd64cbfb48c990
Akil Krishnan <akil.krishnan.SLIB53@gmail.com>
2020-08-18 08:25:40 +0000

    Solve all exercises from quiz 3 through quiz 4

--- exercises/modules/modules1.rs
+++ exercises/modules/modules1.rs
@@ -2,7 +2,7 @@
 // Make me compile! Execute `rustlings hint modules1` for hints :)
 
 mod sausage_factory {
-    fn make_sausage() {
+    pub fn make_sausage() {
         println!("sausage!");
     }
 }
